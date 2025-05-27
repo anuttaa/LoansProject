c057083e772e37c476377e9436d2d25120454913
User <annatyreiko@gmail.com>
2025-05-27 03:27:46 +0300

    Добавлены функции работы с платежами и просмотра кредитов всех клиентов

---
+++ src/main/java/client/viewFactories/AllLoansCreator.java
@@ -0,0 +1,4 @@
+package client.viewFactories;
+
+public class AllLoansCreator {
+}
---
+++ src/main/java/enums/PaymentType.java
@@ -0,0 +1,7 @@
+package Enum;
+
+public enum PaymentType {
+    REGULAR,       // Обычный платеж
+    FULL_PREPAYMENT, // Полное досрочное погашение
+    PARTIAL_PREPAYMENT // Частичное досрочное погашение
+}
